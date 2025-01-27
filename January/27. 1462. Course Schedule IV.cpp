f13f7748cea37adb9ee67d147a2fdb0c83e3fb7b
Abdul-Rahimm <k213951@nu.edu.pk>
2025-01-27 15:36:58 +0500

    course schedule 4. did thru kahns algo. learned floyd warshall and basic techqniues. good practice

---
+++ January/27. 1462. Course Schedule IV.cpp
@@ -0,0 +1,64 @@
+class Solution
+{
+public:
+    vector<bool> checkIfPrerequisite(int numCourses,
+                                     vector<vector<int>> &prerequisites,
+                                     vector<vector<int>> &queries)
+    {
+
+        unordered_map<int, vector<int>> adjList;
+        vector<int> indegree(numCourses, 0);
+
+        for (auto edge : prerequisites)
+        {
+            adjList[edge[0]].push_back(edge[1]);
+            indegree[edge[1]]++;
+        }
+
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++)
+        {
+            if (!indegree[i])
+            {
+                q.push(i);
+            }
+        }
+
+        // Map from the node as key to the set of prerequisite nodes.
+        unordered_map<int, unordered_set<int>> nodePrerequisites;
+
+        while (!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            for (auto adj : adjList[node])
+            {
+                // Add node and prerequisite of node to the prerequisites of adj
+                nodePrerequisites[adj].insert(node);
+
+                for (auto prereq : nodePrerequisites[node])
+                {
+                    nodePrerequisites[adj].insert(prereq);
+                }
+
+                // when u visit a ngbr, reduce their indegree
+                indegree[adj]--;
+
+                // if indefree becomes 0 , enqqueu them
+                if (!indegree[adj])
+                {
+                    q.push(adj);
+                }
+            }
+        }
+
+        vector<bool> answer;
+        for (auto q : queries)
+        {
+            answer.push_back(nodePrerequisites[q[1]].contains(q[0]));
+        }
+
+        return answer;
+    }
+};
