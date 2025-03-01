987250f401d0a09943db88a25ae17f35a6ce8990
Abdul-Rahimm <k213951@nu.edu.pk>
2025-03-01 10:37:28 +0500

    easy array based question. simple simulation

---
+++ March/1. 2460. Apply Operations to an Array.cpp
@@ -0,0 +1,39 @@
+class Solution
+{
+public:
+    vector<int> applyOperations(vector<int> &nums)
+    {
+        int n = nums.size();
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            if (nums[i] == nums[i + 1])
+            {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        int count = 0;
+        for (int i = 0, j = 0; i < n; i++)
+        {
+            nums[j] = nums[i];
+            cout << nums[j] << " " << nums[i] << " ";
+
+            if (nums[i] > 0)
+            {
+                j++;
+                continue;
+            }
+
+            count++;
+        }
+
+        for (int i = n - count; i < n; i++)
+        {
+            nums[i] = 0;
+        }
+
+        return nums;
+    }
+};
